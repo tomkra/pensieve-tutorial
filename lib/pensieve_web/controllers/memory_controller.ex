d04b256d1c8038ca01f9581644823957f15cb47f
Tomáš Král <kralutomas@gmail.com>
2023-12-23 22:12:25 +0100

    add index page

--- lib/pensieve_web/controllers/memory_controller.ex
+++ lib/pensieve_web/controllers/memory_controller.ex
@@ -1,3 +1,11 @@
 defmodule PensieveWeb.MemoryController do
+  use PensieveWeb, :controller
 
+  alias Pensieve.Repo
+  alias Pensieve.Memories.Memory
+
+  def index(conn, _params) do
+    memories = Repo.all(Memory)
+    render(conn, :index, memories: memories)
+  end
 end
---
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -0,0 +1,5 @@
+defmodule PensieveWeb.MemoryHTML do
+  use PensieveWeb, :html
+
+  embed_templates "memory_html/*"
+end
