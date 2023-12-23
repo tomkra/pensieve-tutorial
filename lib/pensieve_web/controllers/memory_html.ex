487d53df928ab179bcbdbe0043018db49ea5add3
Tomáš Král <kralutomas@gmail.com>
2023-12-23 22:34:29 +0100

    update memories index page UI

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -2,4 +2,13 @@ defmodule PensieveWeb.MemoryHTML do
   use PensieveWeb, :html
 
   embed_templates "memory_html/*"
+
+  attr :memories, :list, required: true
+  def information(assigns) do
+    ~H"""
+    <div class="italic mb-4">
+      There are <%= length(@memories) %> memories in your Pensieve
+    </div>
+    """
+  end
 end
