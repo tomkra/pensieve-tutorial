96ffe501f0234b5f1f6eddec700288fe261ea69d
Tomáš Král <kralutomas@gmail.com>
2023-12-23 22:49:06 +0100

    extract MemoryHTML.memory_inserted_at/1

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -1,6 +1,8 @@
 defmodule PensieveWeb.MemoryHTML do
   use PensieveWeb, :html
 
+  alias Pensieve.Memories.Memory
+
   embed_templates "memory_html/*"
 
   attr :memories, :list, required: true
@@ -10,5 +12,9 @@ defmodule PensieveWeb.MemoryHTML do
       There are <%= length(@memories) %> memories in your Pensieve
     </div>
     """
+  end
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
   end
 end
